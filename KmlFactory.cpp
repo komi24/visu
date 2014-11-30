2cd0379f6b3c66e404eba63bd48982d5d48ab667
mickael <bolnet.mickael@gmail.com>
2014-11-30 17:36:16 +0100

    KmlFactory::parseData dev->tester

--- KmlFactory.cpp
+++ KmlFactory.cpp
@@ -5,6 +5,29 @@ using namespace std;
 void KmlFactory::parseData(int day, int hour){
   this->parser = new Parser();
   this->parser->parseFile(day,hour);
+  limitW = parser->stations.begin().longitude;
+  limitE = limitE;
+  limitN = parser->stations.begin().latitude;
+  limitS = limitN;
+  domainMin = parser->stations.begin().value;
+  domainMax = domainMin;
+
+  for(vector<Station>::iterator it = parser->stations.begin();
+      it != parser->stations.end(); it++){
+    if(limitW > it->longitude)
+      limitW = it->longitude;
+    if(limitE < it->longitude)
+      limitE = it->longitude;
+    if(limitN < it->latitude)
+      limitN = it->latitude;
+    if(limitS > it->latitude)
+      limitS = it->latitude;
+    if(domainMin > it->value)
+      dmainMin = it->value;
+    if(domainMax > it->value)
+      domainMax = it->value;
+  }
+
 }
 
 void KmlFactory::setInterp(float(*f)(float,float)){
